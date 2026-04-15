852dcc90863b3ff4c5787a20a79efc1e8b61e604
Eliaspolion <eliaspolion@hotmail.com>
2026-04-15 02:45:54 +0200

    Delete app directory

--- app/app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ElitePerformanceCoachingWebsite from "../ElitePerformanceCoachingWebsite";
-
-export default function Page() {
-  return <ElitePerformanceCoachingWebsite />;
-}
--- app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ElitePerformanceCoachingWebsite from "../ElitePerformanceCoachingWebsite";
-
-export default function Page() {
-  return <ElitePerformanceCoachingWebsite />;
-}// paste your main website code here
